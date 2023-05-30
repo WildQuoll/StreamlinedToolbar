6e32d1a0441c61ba998af871f31f054c995d93b0
WildQuoll <>
2023-05-30 21:56:55 +0100

    Improved compatiblity with Better Healthcare/Education Toolbar mods.
    Depending on mod load order, CCP healthcare/education buildings may not have been shown in any tab.

--- IsCategoryValidPatches.cs
+++ IsCategoryValidPatches.cs
@@ -11,6 +11,11 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(GeneratedScrollPanel), "IsCategoryValid", new Type[] { typeof(BuildingInfo), typeof(bool) })]
     class IsCategoryValidForBuildingPatch
     {
+        // Note: These (postfix) patches are high priority, as it lets other mods, which are likely to use default priority, override our behaviour. 
+        // Most mods are probably going to be more targeted, so it makes sense they would take precedence.
+        // Examples of mods that benefit from this: Better Education Toolbar, Better Healthcare Toolbar.
+
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(BuildingInfo info, bool ignore, GeneratedScrollPanel __instance, ref bool __result, ref string ___m_Category)
         {
@@ -31,6 +36,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(PublicTransportPanel), "IsCategoryValid", new Type[] { typeof(NetInfo), typeof(bool) })]
     class IsCategoryValidForNetworkPatch2
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(NetInfo info, bool ignoreCategories, ref bool __result, ref string ___m_Category)
         {
@@ -51,6 +57,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(GeneratedScrollPanel), "IsCategoryValid", new Type[] { typeof(PropInfo), typeof(bool) })]
     class IsCategoryValidForPropPatch
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(PropInfo info, bool ignore, ref bool __result, ref string ___m_Category)
         {
@@ -71,6 +78,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(BeautificationPanel), "IsCategoryValid", new Type[] { typeof(PropInfo), typeof(bool) })]
     class IsCategoryValidForBeautificationPropPatch
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(PropInfo info, bool ignore, ref bool __result, ref string ___m_Category)
         {
@@ -91,6 +99,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(BeautificationPanel), "IsCategoryValid", new Type[] { typeof(BuildingInfo), typeof(bool) })]
     class IsCategoryValidForBeautificationBuildingPatch
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(BuildingInfo info, bool ignoreCategories, ref bool __result, ref string ___m_Category)
         {
@@ -111,6 +120,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(BeautificationPanel), "IsCategoryValid", new Type[] { typeof(NetInfo), typeof(bool) })]
     class IsCategoryValidForBeautificationNetworkPatch
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(NetInfo info, bool ignoreCategories, ref bool __result, ref string ___m_Category)
         {
@@ -131,6 +141,7 @@ namespace StreamlinedToolbar
     [HarmonyPatch(typeof(BeautificationPanel), "IsCategoryValid", new Type[] { typeof(TreeInfo), typeof(bool) })]
     class IsCategoryValidForTreePatch
     {
+        [HarmonyPriority(Priority.High)]
         [HarmonyPostfix]
         public static void Postfix(TreeInfo info, bool ignore, ref bool __result, ref string ___m_Category)
         {
